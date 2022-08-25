33fdc96ca4bb294d675d03c1c3306e0dfcefd670
jiingler <jinglelin82@gmail.com>
2022-08-25 20:49:37 +0900

    angular upgrade

--- src/app/components/rect/rect.component.spec.ts
+++ src/app/components/rect/rect.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RectComponent } from './rect.component';
 
@@ -6,7 +6,7 @@ describe('RectComponent', () => {
   let component: RectComponent;
   let fixture: ComponentFixture<RectComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RectComponent ]
     })
